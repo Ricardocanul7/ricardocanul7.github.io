8c63acf681329ca444c073cfd8b6d682939ec949
Ricardo Canul <ricardocanul7@gmail.com>
2021-08-25 11:33:21 +0200

    Add skills section

--- js/load-data.js
+++ js/load-data.js
@@ -22,4 +22,31 @@ fetch('./js/data/projects.json')
 
             container.innerHTML = rawHTML;
         })
+    });
+
+/* Load Skils */
+fetch('./js/data/skills.json')
+    .then(response => {
+        response.json().then((data) => {
+            let rawHTML = '';
+            const container = document.getElementById('skill-list');
+
+            data.forEach(item => {
+                rawHTML += `
+                <div class="col-lg-3 col-md-6 mb-5">
+                    <span class="service-icon rounded-circle mx-auto mb-3">
+                        <i class="${item.fawesome_icon}"></i>
+                    </span>
+                    <h4>
+                        <strong>${item.title}</strong>
+                    </h4>
+                    <p class="text-faded mb-0">
+                        ${item.description}
+                    </p>
+                </div>
+                `;
+            });
+
+            container.innerHTML = rawHTML;
+        })
     })
