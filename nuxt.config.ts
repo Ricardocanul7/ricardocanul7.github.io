047f1149d62359f98a6e9f698dfacf85357b15d4
Ricardo Canul <ricardocanul7@gmail.com>
2026-06-08 14:01:55 +0200

    fix: add baseURL configuration to Nuxt app settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/portfolio/'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
