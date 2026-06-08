28ab8ee79574beb94efb7e56e1e188bd5b8f16d4
Ricardo Canul <ricardocanul7@gmail.com>
2026-06-08 15:37:09 +0200

    refactor: remove custom baseURL configuration from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/'
-  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
