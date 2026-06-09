186f726a4d8e96a372211f02b1a33a2ebb693baa
Ricardo Canul <ricardocanul7@gmail.com>
2026-06-09 13:19:29 +0200

    fix: update baseURL in nuxt.config.ts for local development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    baseURL: 'https://ricardocanul7.github.io/portfolio/',
+    baseURL: 'portfolio/',
     head: {
       title: 'Portfolio',
       meta: [
