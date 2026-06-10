802f34a53c4b0d74b8f2455d55a835ca17523f0a
Ricardo Canul <ricardocanul7@gmail.com>
2026-06-10 19:13:24 +0200

    feat: refactor project image paths to use a centralized images directory

--- server/api/projects.js
+++ server/api/projects.js
@@ -1,101 +1,103 @@
 export default defineEventHandler((event) => {
+    const imagesDirectory = `/images/projects/`
+
     return {
         projects: [
             {
                 "title": "BC Studio",
                 "description": "Real state Website",
-                "image": "/images/projects/bc-studio.png",
+                "image": `${imagesDirectory}bc-studio.png`,
                 "link": "https://bcstudio.com.mx/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "GOmart",
                 "description": "Store catalog",
-                "image": "/images/projects/gomart.png",
+                "image": `${imagesDirectory}gomart.png`,
                 "link": "https://gomart.com.mx/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Torneo de Golf",
                 "description": "Website for golf tournament",
-                "image": "/images/projects/torneo-de-golf-2025.png",
+                "image": `${imagesDirectory}torneo-de-golf-2025.png`,
                 "link": "https://torneogolfccq.com/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Nenis Club",
                 "description": "Website for a company that helps women entrepreneurs",
-                "image": "/images/projects/nenisclub.png",
+                "image": `${imagesDirectory}nenisclub.png`,
                 "link": "https://nenisclub.mx/",
                 "tags": ['HubSpot', 'HTML', 'SCSS', 'JavaScript']
             },
             {
                 "title": "Inmobiliaria110",
                 "description": "Real state Website",
-                "image": "/images/projects/inmobiliaria110.png",
+                "image": `${imagesDirectory}inmobiliaria110.png`,
                 "link": "https://inmobiliaria110.com/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Sistemico",
                 "description": "SaaS website for a company that helps organizing documents",
-                "image": "/images/projects/sistemico.png",
+                "image": `${imagesDirectory}sistemico.png`,
                 "link": "https://sistemico.mx/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "ASSO Desarrollos",
                 "description": "Real state website",
-                "image": "/images/projects/asso-desarrollos.png",
+                "image": `${imagesDirectory}asso-desarrollos.png`,
                 "link": "https://assodesarrollo.com/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'Vue 2', 'CakePHP']
             },
             {
                 "title": "Electromax",
                 "description": "Landing page for electronics store",
-                "image": "/images/projects/electromax.png",
+                "image": `${imagesDirectory}electromax.png`,
                 "link": "https://electromax.com.mx/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Landing - Triventto",
                 "description": "Real estate landing",
-                "image": "/images/projects/triventto-2.png",
+                "image": `${imagesDirectory}triventto-2.png`,
                 "link": "https://triventto.mx",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Landing - Caminata",
                 "description": "Real estate landing",
-                "image": "/images/projects/caminata.png",
+                "image": `${imagesDirectory}caminata.png`,
                 "link": "https://caminata.c-urbano.com",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Delivery Store - Cuantica Pizza",
                 "description": "Food online store",
-                "image": "/images/projects/cuanticapizza.png",
+                "image": `${imagesDirectory}cuanticapizza.png`,
                 "link": "https://cuanticapizza.com",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Primus",
                 "description": "Real estate website with admin panel",
-                "image": "/images/projects/primus.png",
+                "image": `${imagesDirectory}primus.png`,
                 "link": "https://primusrealtygroup.com/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Decomarmol",
                 "description": "Materials website with admin panel",
-                "image": "/images/projects/decomarmol.png",
+                "image": `${imagesDirectory}decomarmol.png`,
                 "link": "https://decomarmol.com.mx/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             },
             {
                 "title": "Terrakaan",
                 "description": "Real state website",
-                "image": "/images/projects/terrakaan.png",
+                "image": `${imagesDirectory}terrakaan.png`,
                 "link": "https://terrakaan.org/",
                 "tags": ['PHP', 'HTML', 'SCSS', 'JavaScript', 'CakePHP']
             }
